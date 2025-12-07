f8392fa25bc5b62268c257054d755940e7204e3e
Ahmed <ahmedboyman9@gmail.com>
2025-12-07 02:19:54 +0200

    Updated something

--- Announcement.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Announcements (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255),
-    content TEXT,
-    instructor_id INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (instructor_id) REFERENCES users(id)
-);
--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from controllers.AnnouncementController import create_announcement, list_announcements
-
-app = Flask(__name__)
-app.secret_key = "secret-phase4"  # Required for session
-
-
-app.add_url_rule('/announcement/create', view_func=create_announcement, methods=['GET', 'POST'])
-app.add_url_rule('/announcements', view_func=list_announcements, methods=['GET'])
